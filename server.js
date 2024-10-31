62aae73ae2b42c5a38aace06d3f795fc7532a687
shawmonika616ms <shawmonika616ms@gmail.com>
2024-10-31 21:40:40 +0530

    Description : Update User workflow.

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.get('/add-user', (req, res) => {
     res.render('add_user')
 })
 
+app.get('/update-user', (req, res) => {
+    res.render('update_user')
+})
+
 app.listen(PORT, (req, res) => {
     console.log("listening on");
 });
