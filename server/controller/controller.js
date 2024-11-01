b94d2c8bb8eb27e7130bbbc7be8b16306571fc37
shawmonika616ms <shawmonika616ms@gmail.com>
2024-11-01 17:50:15 +0530

    Description :  defined all APIs.

---
+++ server/controller/controller.js
@@ -0,0 +1,20 @@
+var userdb = require('../model/model')
+
+//create and save new user
+
+exports.create = (req, res) => {
+
+}
+
+exports.find = (req, res) => {
+
+}
+
+exports.update = (req, res) => {
+
+}
+
+exports.delete = (req, res) => {
+
+}
+
